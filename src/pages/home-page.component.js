b30df30f185ffd7b8d2f607985d9b0af8641d341
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-06 10:19:54 +0530

    ok
    ok

---
+++ src/pages/home-page.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+
+    //functional component of React
+    return(
+        <div className="HomePage">
+            <p>This is the home page component</p>
+        </div>
+    )
+
+}
+
+export default HomePage;
