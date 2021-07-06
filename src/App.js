0a8b762fd510cd3cc20539abc80750378ee880e6
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-06 11:46:37 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import HomePage from './pages/home-page-component/home-page.component'
-import SignInPage from './pages/signin-page-component/signin-page.component'
+import "./App.css";
+import HomePage from "./pages/home-page-component/home-page.component";
+import SignInPage from "./pages/signin-page-component/signin-page.component";
+import { Route } from "react-router-dom";
+import CartPage from "./pages/cart-page-component/cart-page-component";
 
 function App() {
   return (
     <div className="App">
-       <HomePage />
-       <SignInPage/>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/signin" component={SignInPage} />
+      <Route path="/signin/cart" component={CartPage} />
     </div>
   );
 }
