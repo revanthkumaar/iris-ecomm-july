fee81fa7aceeb568c22ad16f82a25e2361f3bd03
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-06 11:04:22 +0530

    da
    adasd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HomePage from './pages/home-page-component/home-page.component'
+import SignInPage from './pages/signin-page-component/signin-page.component'
 
 function App() {
   return (
     <div className="App">
        <HomePage />
+       <SignInPage/>
     </div>
   );
 }
