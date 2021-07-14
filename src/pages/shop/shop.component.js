fce770b56bc702ca71810f817ea0af11d3138cdd
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-14 09:46:56 +0530

    shop data added

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import SHOP_DATA from "./shop.data.js";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component"
+
+import CollectionPreview from "../../components/collection-preview/collection-preview";
 
 class ShopPage extends React.Component {
   constructor(props) {
