c75209f155b2dfc3c1dd99de8b05ed7fb3e75f6e
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-16 12:34:50 +0530

    lazy loading added

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,9 +1,9 @@
-import React from "react";
-
+import React, {lazy, Suspense} from "react";
 import SHOP_DATA from "./shop.data.js";
-
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+//import ShopTop from '../../components/shop-page-top.component'
 
+const ShopTop = lazy(() => import("../../components/shop-page-top.component"));
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
@@ -16,11 +16,18 @@ class ShopPage extends React.Component {
   render() {
     const { collections } = this.state;
     return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
+      <>
+        <div className="SomePreview">
+          <Suspense fallback={<div>Loading... top component</div>}>
+            <ShopTop />
+          </Suspense>
+        </div>
+        <div className="shop-page">
+          {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+          ))}
+        </div>
+      </>
     );
   }
 }
